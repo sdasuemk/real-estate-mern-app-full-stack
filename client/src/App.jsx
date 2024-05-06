71ce6710ca6a00d2262f0d0fa84cfcc7ce94c130
Soumya Das <sdasuemk@gmail.com>
2024-05-06 23:59:36 +0530

    initial routes added, Nav bar created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
-    <h1 className="text-red-500">App</h1>
-  )
-}
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
