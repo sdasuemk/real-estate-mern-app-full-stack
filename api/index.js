a6cbed5f967aac7d6a468c90067890def58ae20c
Soumya Das <sdasuemk@gmail.com>
2024-05-25 15:42:45 +0530

    signup-route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import connectDB from './db/db.js';
 // import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -18,8 +19,20 @@ app.use(express.json());
 
 // Routers
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/auth", authRouter);
 
 //Listen
 app.listen(process.env.PORT || 4000, () => {
     console.log(`listening on port ${process.env.PORT || 4000} :)`);
 })
+
+// Error handlers
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
