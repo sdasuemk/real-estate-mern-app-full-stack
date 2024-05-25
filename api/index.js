e749f99c311901ca220b5a8de498a0f684a3d449
Soumya Das <sdasuemk@gmail.com>
2024-05-25 17:43:27 +0530

    sign-in route API

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import connectDB from './db/db.js';
 // import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
@@ -15,6 +16,7 @@ const app = express();
 
 
 app.use(express.json());
+app.use(cors());
 
 
 // Routers
