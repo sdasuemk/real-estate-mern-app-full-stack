96f9e1d0d97791cc2b5eefabb95e0ba2850ccab7
Soumya Das <sdasuemk@gmail.com>
2024-05-25 14:53:00 +0530

    create a test API route

--- api/index.js
+++ api/index.js
@@ -1,17 +1,25 @@
 import express from 'express';
 import connectDB from './db/db.js';
 // import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
 // db connection settings
 connectDB();
 
-
-
+//Create Application
 const app = express();
+
+
 app.use(express.json());
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`listening on port ${process.env.PORT || 3000} :)`);
+
+// Routers
+app.use("/api/v1/user", userRouter);
+
+//Listen
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`listening on port ${process.env.PORT || 4000} :)`);
 })
