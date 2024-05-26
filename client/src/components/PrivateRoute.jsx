892003bb3e88bb0227a71fe0225b5fd61d877b69
Soumya Das <sdasuemk@gmail.com>
2024-05-26 13:46:04 +0530

    update the header and the profile page made private (#9)
    * redux-persist added
    
    * firebase authentication added for google login
    
    * update the header and the profile page made private
    
    * profile page UI

---
+++ client/src/components/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+import { useSelector } from 'react-redux';
+import { Outlet, Navigate } from 'react-router-dom';
+
+const PrivateRoute = () => {
+    const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div>
+        {currentUser ? <Outlet /> : <Navigate to='/sign-in' />}
+    </div>
+  )
+}
+
+export default PrivateRoute
