e5d65049a4597f72e240083e4010df7036bc471c
Soumya Das <sdasuemk@gmail.com>
2024-06-01 17:52:48 +0530

    Listing create, update (#11)
    * create, update listing

---
+++ api/routes/listing.route.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { createListing, deleteListing, updateListing, getListing, getListings } from '../controllers/listing.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
+
+const router = express.Router();
+
+router.post('/create-listing', verifyToken, createListing);
+router.delete('/delete-listing/:id', verifyToken, deleteListing);
+router.post('/update-listing/:id', verifyToken, updateListing);
+router.get('/get-a-listing/:id', getListing);
+router.get('/get-listing', getListings);
+
+export default router;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { About, Home, SignIn, SignUp, Profile } from "./pages/index";
+import { About, Home, SignIn, SignUp, Profile, CreateListing, UpdateListing, Listing } from "./pages/index";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 
@@ -12,8 +12,11 @@ const App = () => {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
+        <Route path="/listing/:listingId" element={<Listing />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/update-listing/:id" element={<UpdateListing />} />
         </Route>
       </Routes>
     </BrowserRouter>
